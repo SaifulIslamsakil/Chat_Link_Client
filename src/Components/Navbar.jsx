417ebb90f322e21b8c9f58d7923e96b9be9f5d4a
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-04-02 01:46:39 +0600

    added to footer section in home pages

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,13 +15,13 @@ const Navbar = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content text-black mt-3 z-[1] p-5 space-y-4 shadow bg-base-100 rounded-box w-52 cursor-pointer">
                         {manu}
                     </ul>
                 </div>
-                <div className=" flex items-centers gap-1">
+                <div className="items-centers gap-1 hidden md:flex">
                     <span className=" text-5xl"><BiMessageRoundedDots /></span>
                     <p className=" text-3xl font-semibold cursor-pointer">Chat Link</p>
                 </div>
